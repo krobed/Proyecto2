d0753ff4310d481c2b52e78390109d950d920d7d
krobed <73363947+krobed@users.noreply.github.com>
2024-06-30 22:52:48 -0400

    Update format_text.py

--- format_text.py
+++ format_text.py
@@ -5,5 +5,39 @@ def format_text(text:str, maxwidth:int, file_name= '') -> List:
     '''
     if file_name== '':
         file_name=input('Nombre del archivo: ')
+    n=len(text)
+	result = 0
+	len_mat = np.zeros(n) 
+	currRow = 0
+    for j in range(n) :       
+        if j==0 : 
+            len_mat[currRow] = 0
+            strings.append(text[j])
+        
+        elif (text[j - 1] != ' ') :	
+            len_mat[currRow] = len_mat[currRow] + 1
+            result = max(result, len_mat[currRow])
+        else : 
+            currRow+=1
+            len_mat[currRow] = 0 
     
     
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
