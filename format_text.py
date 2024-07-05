84835d1b0ef3e3c1684d8ca328cf7821282816d5
krobed <73363947+krobed@users.noreply.github.com>
2024-07-05 17:43:05 -0400

    Update format_text.py
    se modifica la funcion format_text version final con pruebas

--- format_text.py
+++ format_text.py
@@ -1,29 +1,35 @@
-def format_text(text:str, maxwidth:int, file_name= '') -> List:
+import word_wrap
+import common_substring
+import word_break
+
+
+
+def format_text(text:str, maxwidth:int) -> list:
     ''' Este código recibe un string y un int para luego correr un algoritmo
         que se encarga de agrupar fragmentos del texto en una lista,
-        dependiendo del valor de maxwidth. Retorna la lista.
+        dependiendo del valor de maxwidth. Retorna la lista y la ingresa a un archivo de texto llamado 'archivo'.
     '''
-    if file_name== '':
-        file_name=input('Nombre del archivo: ')
-    n=len(text)
-	result = 0
-	len_mat = np.zeros(n) 
-	currRow = 0
-    for j in range(n) :       
-        if j==0 : 
-            len_mat[currRow] = 0
-            strings.append(text[j])
-        
-        elif (text[j - 1] != ' ') :	
-            len_mat[currRow] = len_mat[currRow] + 1
-            result = max(result, len_mat[currRow])
-        else : 
-            currRow+=1
-            len_mat[currRow] = 0 
     
-    
+    text = text.split(' ')
+    dictionary = [text[0]]
+    for i in text[1:]:
+        if len(i)+len(dictionary[-1])+1 <=maxwidth:
+            dictionary[-1] = dictionary[-1] + ' '+ i
+        else:
+            dictionary.append(i)
+    file = open('archivo.txt', 'w')
+    for i in dictionary:
+        file.write(i+'\n')
+    return dictionary
 
-        
+
+
+
+
+text = 'hoy es un día con mucho calor'
+print(format_text(text, 6))
+text= 'this is a programming problem which demonstrates dynamic programming'
+print(format_text(text,15))
 
 
 
